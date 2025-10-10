ff71b1086c22c18207633afb50c1baf62724caf5
mizotii <mztiiiii0@gmail.com>
2025-10-09 22:12:31 -0700

    finish

---
+++ calculator.js
@@ -0,0 +1,27 @@
+function add(x, y) {
+	return x + y;
+}
+
+const subtract = function(x, y) {
+	return x - y;
+}
+
+const multiply = (x, y) => x * y;
+
+const divide = (x, y) => y == 0 ? "Cannot divide by zero" : x / y;
+
+const calculate = function(a, b, operation) {
+	switch(operation) {
+		case "+":
+			return add(a, b);	
+		case "-":
+			return subtract(a, b);
+		case "*":
+			return multiply(a, b);
+		case "/":
+			return divide(a, b);
+		default:
+			return "Invalid operator."
+	}
+}
+
